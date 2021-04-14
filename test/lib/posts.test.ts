aabb1e1f8ecee66e5c6643bc0326af1f0c7fb913
autopp <autopp.inc@gmail.com>
2021-04-14 23:42:00 +0900

    use jest

---
+++ test/lib/posts.test.ts
@@ -0,0 +1,24 @@
+import { readdirSync } from "fs"
+import { getAllPostIds } from "../../src/lib/posts"
+
+jest.mock("fs", () => ({
+  readdirSync: jest.fn(),
+}))
+const mockReaddirSync = (readdirSync as unknown) as jest.Mock<
+  string[],
+  [string]
+>
+
+describe("getAllPostIds", () => {
+  beforeEach(() => {
+    mockReaddirSync.mockReturnValue(["first.md", "second.md"])
+  })
+
+  it("returns filenames without ext as id", async () => {
+    const ids = await getAllPostIds()
+    expect(ids).toEqual([
+      { params: { id: "first" } },
+      { params: { id: "second" } },
+    ])
+  })
+})
